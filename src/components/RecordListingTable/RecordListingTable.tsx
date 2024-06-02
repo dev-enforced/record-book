d917b37bd764c7bcd0cb6c47429d207dd78f3ac8
Roshan J. <devenforced@gmail.com>
2024-06-02 11:53:04 +0530

    refactor: :recycle: Introduced recordlisting table rows

--- src/components/RecordListingTable/RecordListingTable.tsx
+++ src/components/RecordListingTable/RecordListingTable.tsx
@@ -1,5 +1,34 @@
+import { RECORDS_LISTING_COLUMNS } from "@/data/recordsListing.config";
+import styles from "./RecordListingTable.module.css";
+
+const RecordListingTableColumns = () => {
+  return (
+    <thead className={styles.record_listing_table_header}>
+      <tr className={styles.record_listing_table_header_row}>
+        {RECORDS_LISTING_COLUMNS.map(({ label, value }) => {
+          return (
+            <th
+              key={`${label}-${value}`}
+              className={styles.record_listing_table_header_column_wrapper}
+            >
+              <span>{label}</span>
+            </th>
+          );
+        })}
+      </tr>
+    </thead>
+  );
+};
+
 const RecordListingTable = () => {
-  return <div>ListingTable</div>;
+  return (
+    <section className={styles.record_listing_table_wrapper}>
+      <table className={styles.record_listing_table}>
+        <RecordListingTableColumns />
+        <tbody></tbody>
+      </table>
+    </section>
+  );
 };
 
 export default RecordListingTable;
